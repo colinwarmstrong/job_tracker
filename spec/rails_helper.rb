53e9aa04197b2d8104dfc756ad00fc49d3216bab
Colin Armstrong <colinwarmstrong@gmail.com>
2018-12-17 16:56:38 -0700

    Update dependencies to address possible security vulnerabilities

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |c|
     DatabaseCleaner.clean #after each test cleans
   end
 end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
